a13df9624b04ab2daab7bb63eaa1bab43f639e45
wdmuer <warddemuer@hotmail.com>
2025-10-13 19:22:14 +0200

    feat: new endpoint that retrieves task_uri from request and initiates processing

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,7 @@ LABEL maintainer="ward@ml2grow.com"
 
 RUN hf download svercoutere/RoBERTa-NER-BE-Loc
 ENV NER_MODEL_PATH=/root/.cache/huggingface/hub/models--svercoutere--RoBERTa-NER-BE-Loc/snapshots/423e85a3f6be1511335e9a46f4a120af046dcda5
+
+ENV EXPECTED_TASK_PREDICATE=http://www.w3.org/ns/adms#status
+ENV EXPECTED_TASK_OBJECT=http://redpencil.data.gift/id/concept/JobStatus/scheduled
+ENV BASE_REGISTRY_URI=https://api.basisregisters.vlaanderen.be
--- web.py
+++ web.py
@@ -4,11 +4,9 @@ from src.pipeline import run_pipeline
 from src.spacy_ner_analyzer import SpacyNERAnalyzer
 from src.nominatim_geocoder import NominatimGeocoder
 
-from flask import jsonify, copy_current_request_context
+from flask import jsonify, copy_current_request_context, request
 import threading
 
-
-# TODO: DOWNLOAD NER MODEL?
 
 ner_analyzer = SpacyNERAnalyzer(model_path=os.getenv("NER_MODEL_PATH"),
                                 labels=json.loads(os.getenv("NER_LABELS")))
@@ -16,11 +14,29 @@ geocoder = NominatimGeocoder(base_url=os.getenv("NOMINATIM_BASE_URL"),
                              rate_limit=0.5)
 
 
-@app.route("/notify-change")
-def notify_change():
-    @copy_current_request_context
-    def run_pipeline_with_context():
-        run_pipeline(ner_analyzer, geocoder)
+@app.post("/delta")
+def delta():
+    data = request.get_json(silent=True) or []
 
-    threading.Thread(target=run_pipeline_with_context, daemon=True).start()
-    return jsonify({"status": "accepted", "message": "Processing started"}), 202
+    for patch in data:
+        for ins in patch.get("inserts", []):
+            if ins != []:
+                subj = ins.get("subject", {}).get("value")
+                pred = ins.get("predicate", {}).get("value")
+                obj = ins.get("object", {}).get("value")
+
+                if pred != os.getenv("EXPECTED_TASK_PREDICATE") or obj != os.getenv("EXPECTED_TASK_OBJECT"):
+                    return jsonify({
+                        "error": "Unexpected predicate/object",
+                        "got": {"predicate": pred, "object": obj},
+                        "expected": {"predicate": os.getenv("EXPECTED_TASK_PREDICATE"), "object": os.getenv("EXPECTED_TASK_OBJECT")}
+                    }), 400
+
+                else:
+                    @copy_current_request_context
+                    def run_pipeline_with_context():
+                        run_pipeline(subj, ner_analyzer, geocoder)
+
+                    threading.Thread(
+                        target=run_pipeline_with_context, daemon=True).start()
+                    return jsonify({"status": "accepted", "message": "Processing started"}), 202
